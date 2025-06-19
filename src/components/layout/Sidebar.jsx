bb9fad94a952a8e608b3cc60a085648724fb2198
Aleksei Orlov <alexeyorlov65@gmail.com>
2025-06-20 02:52:23 +0300

    remove unused

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -6,22 +6,22 @@ export default function Sidebar() {
             <h2 className="font-semibold text-lg mb-4">Quick Links</h2>
             <ul className="space-y-2">
                 <li>
-                    <a href="#" className="text-blue-600 hover:underline">
+                    <a href="/" className="text-blue-600 hover:underline">
                         Friends
                     </a>
                 </li>
                 <li>
-                    <a href="#" className="text-blue-600 hover:underline">
+                    <a href="/" className="text-blue-600 hover:underline">
                         Groups
                     </a>
                 </li>
                 <li>
-                    <a href="#" className="text-blue-600 hover:underline">
+                    <a href="/" className="text-blue-600 hover:underline">
                         Events
                     </a>
                 </li>
                 <li>
-                    <a href="#" className="text-blue-600 hover:underline">
+                    <a href="/" className="text-blue-600 hover:underline">
                         Marketplace
                     </a>
                 </li>
